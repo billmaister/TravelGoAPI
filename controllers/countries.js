95a07ebcfef72e2f9b379bb51fd259a36b7926cb
Sergei Kaukiainen <Sergei.Kaukiainen@roimaint.com>
2022-11-24 11:15:25 +0200

    changed return json for login and register endpoints, also some refactoring by Prettier extension

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,16 +1,18 @@
-const Country = require('../models/Countries')
-const User = require('../models/Users');
-const Recommendation = require('../models/Recommendations')
-const { ObjectId } = require('mongodb');
+const Country = require("../models/Countries");
+const User = require("../models/Users");
+const Recommendation = require("../models/Recommendations");
+const { ObjectId } = require("mongodb");
 
 const getCountries = async (req, res) => {
   try {
     const { continent } = req.params;
-    const countries = await Country.find({ continent: continent.toLowerCase() });
-    if(!countries){
+    const countries = await Country.find({
+      continent: continent.toLowerCase(),
+    });
+    if (!countries) {
       return res.status(404).json({
-        msg: `Did not find any countries. Please check continent value and try again: ${ continent }`
-      })
+        msg: `Did not find any countries. Please check continent value and try again: ${continent}`,
+      });
     }
     res.status(200).json({
       success: true,
@@ -23,17 +25,17 @@ const getCountries = async (req, res) => {
 
 const getCountryDetails = async (req, res) => {
   try {
-    const {country} = req.params;
-    const countryInfo = await Country.findOne({name: country.toLowerCase()})
-    if(!countryInfo) {
+    const { country } = req.params;
+    const countryInfo = await Country.findOne({ name: country.toLowerCase() });
+    if (!countryInfo) {
       return res.status(404).json({
-        msg:`Can't find any country with name: ${country}`
-      })
+        msg: `Can't find any country with name: ${country}`,
+      });
     }
     res.status(200).json({
-        success: true,
-        data: countryInfo,
-      })
+      success: true,
+      data: countryInfo,
+    });
   } catch (error) {
     res.status(500).json({ msg: `Error occured: ${error}` });
   }
@@ -41,65 +43,76 @@ const getCountryDetails = async (req, res) => {
 
 const getRecommendedCountries = async (req, res) => {
   try {
-    const { currentRecommendations } = await Recommendation.findOne({ type: "countries" });
-    const recommendations = await Country.find({ _id: { $in: currentRecommendations }})
-    res.status(200).json({
-        success: true,
-        data: recommendations,
+    const { currentRecommendations } = await Recommendation.findOne({
+      type: "countries",
     });
-    } catch (error) {
-      res.status(500).json({ msg: `Error occured: ${error}` });
-    }
+    const recommendations = await Country.find({
+      _id: { $in: currentRecommendations },
+    });
+    res.status(200).json({
+      success: true,
+      data: recommendations,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: `Error occured: ${error}` });
+  }
 };
 
 const getFavouriteCountries = async (req, res) => {
   try {
-    const { user: { userID } } = req;
-    const { favouriteCountries } = await User.findOne({ _id : ObjectId(userID)}, { favouriteCountries: 1});
-    const completeCountryInformation = await Country.find({ _id: { $in: favouriteCountries }})
-    res.status(200).json({
-        success: "success",
-    data: completeCountryInformation,
+    const {
+      user: { userID },
+    } = req;
+    const { favouriteCountries } = await User.findOne(
+      { _id: ObjectId(userID) },
+      { favouriteCountries: 1 }
+    );
+    const completeCountryInformation = await Country.find({
+      _id: { $in: favouriteCountries },
     });
-    } catch (error) {
-      res.status(500).json({ msg: `Error occured: ${error}` });
-    }
+    res.status(200).json({
+      success: "success",
+      data: completeCountryInformation,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: `Error occured: ${error}` });
+  }
 };
 
 const addCountryToFavourites = async (req, res) => {
   try {
     const {
       user: { userID },
-      body: { id:countryID}
+      body: { id: countryID },
     } = req;
     await User.findByIdAndUpdate(
-      { _id:userID},
-      { $push: { favouriteCountries : countryID }}
-    )
+      { _id: userID },
+      { $push: { favouriteCountries: countryID } }
+    );
     res.status(201).json({
-        success: true,
+      success: true,
     });
-    } catch (error) {
-        res.status(500).json({ msg: `Error occured: ${error}` });
-    }
+  } catch (error) {
+    res.status(500).json({ msg: `Error occured: ${error}` });
+  }
 };
 
 const removeCountryFromFavourites = async (req, res) => {
   try {
     const {
-        user: { userID },
-        body: { id:countryID}
+      user: { userID },
+      body: { id: countryID },
     } = req;
     await User.findByIdAndUpdate(
-        { _id:userID},
-        { $pull: { favouriteCountries : countryID }}
-    )
+      { _id: userID },
+      { $pull: { favouriteCountries: countryID } }
+    );
     res.status(201).json({
-        success: true,
+      success: true,
     });
-    } catch (error) {
-      res.status(500).json({ msg: `Error occured: ${error}` });
-    }
+  } catch (error) {
+    res.status(500).json({ msg: `Error occured: ${error}` });
+  }
 };
 
 module.exports = {
--- models/Users.js
+++ models/Users.js
@@ -1,62 +1,62 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { ObjectId } = require('mongodb');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ObjectId } = require("mongodb");
 
 const UsersSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'Email was not provided.'],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email',
-        ],
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide password'],
-    },
-    firstname: {
-        type: String,
-        required: [true, 'First name was not provided.'],
-        lowercase: true,
-        trim: true,
-        minLength: [2, "First name is too short."],
-        maxLength: [50, "First name is too long."]
-    },
-    lastname: {
-        type: String,
-        required: [true, 'Last name was not provided.'],
-        lowercase: true,
-        trim: true,
-        minLength: [2, "Last name is too short."],
-        maxLength: [50, "Last name is too long."]
-    },
-    favouriteCountries: [{ type : ObjectId }],
-    favouriteCities: [{ type : ObjectId }],
-    favouritePlaces: [{ type : ObjectId }],
-    favouriteEvents: [{ type : ObjectId }]
+  email: {
+    type: String,
+    required: [true, "Email was not provided."],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: [true, "Email already exists"],
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+  firstname: {
+    type: String,
+    required: [true, "First name was not provided."],
+    lowercase: true,
+    trim: true,
+    minLength: [2, "First name is too short."],
+    maxLength: [50, "First name is too long."],
+  },
+  lastname: {
+    type: String,
+    required: [true, "Last name was not provided."],
+    lowercase: true,
+    trim: true,
+    minLength: [2, "Last name is too short."],
+    maxLength: [50, "Last name is too long."],
+  },
+  favouriteCountries: [{ type: ObjectId }],
+  favouriteCities: [{ type: ObjectId }],
+  favouritePlaces: [{ type: ObjectId }],
+  favouriteEvents: [{ type: ObjectId }],
 });
 
-UsersSchema.pre('save', async function() {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+UsersSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-UsersSchema.methods.createToken = function() {
-    return jwt.sign(
-        { userID:this._id, firstname:this.firstname}, 
-        process.env.JWT_SECRET, 
-        { expiresIn: process.env.JWT_LIFETIME }
-    );
+UsersSchema.methods.createToken = function () {
+  return jwt.sign(
+    { userID: this._id, firstname: this.firstname },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
 };
 
-UsersSchema.methods.validatePassword = async function(passwordToValidate) {
-    const isValid = await bcrypt.compare(passwordToValidate, this.password);
-    return isValid;
+UsersSchema.methods.validatePassword = async function (passwordToValidate) {
+  const isValid = await bcrypt.compare(passwordToValidate, this.password);
+  return isValid;
 };
 
-module.exports = mongoose.model('User', UsersSchema);
+module.exports = mongoose.model("User", UsersSchema);
